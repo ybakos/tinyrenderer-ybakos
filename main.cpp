8cf254eb32aac27f398002b633701e645f3e634b
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-03-10 13:44:41 -0700

    Lesson 1: Render the wireframe.
    Extract some constants and rename.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-#include "tgaimage.h"
+#include <vector>
 #include <cmath>
+#include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
+const int WIDTH = 800;
+const int HEIGHT = 800;
+const char* DEFAULT_OBJ_FILENAME = "obj/african_head.obj";
+const char* IMAGE_OUTPUT_FILENAME = "output.tga";
+const TGAColor WHITE = TGAColor(255, 255, 255, 255);
+const TGAColor RED = TGAColor(255, 0, 0, 255);
+
+Model* model;
 
 // True when x changes less than y.
 bool isSteep(int x0, int y0, int x1, int y1) {
@@ -55,11 +64,26 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char* argv[]) {
-  TGAImage image(100, 100, TGAImage::RGB);
-  line(13, 20, 80, 40, image, white);
-  line(20, 13, 40, 80, image, red);
-  line(80, 40, 13, 20, image, red);
-  //image.flip_vertically(); // Origin at left bottom. Why?
-  image.write_tga_file("output.tga");
+  if (argc == 2) {
+    model = new Model(argv[1]);
+  } else {
+    model = new Model(DEFAULT_OBJ_FILENAME);
+  }
+  TGAImage image(WIDTH, HEIGHT, TGAImage::RGB);
+  for (int i = 0; i < model->nfaces(); ++i) {
+    std::vector<int> face = model->face(i);
+    for (int j = 0; j < 3; ++j) {
+      Vec3f v0 = model->vert(face[j]);
+      Vec3f v1 = model->vert(face[(j+1)%3]);
+      int x0 = (v0.x + 1) * WIDTH/2;
+      int y0 = (v0.y + 1) * HEIGHT/2;
+      int x1 = (v1.x + 1) * WIDTH/2;
+      int y1 = (v1.y + 1) * HEIGHT/2;
+      line(x0, y0, x1, y1, image, WHITE);
+    }
+  }
+  image.flip_vertically();
+  image.write_tga_file(IMAGE_OUTPUT_FILENAME);
+  delete model;
   return 0;
 }
