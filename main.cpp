123910c860ab22875759408661a6198dec31b9fb
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-03-09 20:50:53 -0800

    Lesson 1: Implement naive second line attempt, using x as number of steps.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-  for (float t = 0; t < 1.0; t += 0.01) {
-    int x = x0 + (x1-x0)*t;
+  for (int x = x0; x <= x1; ++x) {
+    float t = (x - x0) / (float)(x1 - x0);
     int y = y0 + (y1-y0)*t;
     image.set(x, y, color);
   }
@@ -14,6 +14,8 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 int main(int argc, char* argv[]) {
   TGAImage image(100, 100, TGAImage::RGB);
   line(13, 20, 80, 40, image, white);
+  line(20, 13, 40, 80, image, red);
+  line(80, 40, 13, 20, image, red);
   //image.flip_vertically(); // Origin at left bottom. Why?
   image.write_tga_file("output.tga");
   return 0;
